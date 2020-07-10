340206624d9d4c6885d8ae92e6e6ecf6899ec45f
sadjoker12 <67815824+sadjoker12@users.noreply.github.com>
2020-07-09 21:59:18 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['ID'];
+const myid = ['629272081858691082'];
 const prefix = ['$'];
 const client = new Discord.Client();
 const client2 = new Discord.Client();
